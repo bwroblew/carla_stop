8b2a77fbc437af8586d0a4b8b87881b75b5f9ca5
bwroblew <bwroblewski10@gmail.com>
2019-04-29 18:45:11 +0200

    basic us and filter implementation

--- StopDetector.py
+++ StopDetector.py
@@ -22,14 +22,20 @@ class BndBox:
         self.area = self.width * self.height
 
     def get_cent_dist(self, x, y):
+        """
+        Calculates the distance between point x,y and center of the box
+        :param x: x parameter of point
+        :param y: y parameter of point
+        :return: distance from point to center of box
+        """
         return math.sqrt((self.center_x - x) ** 2 + (self.center_y - y) ** 2)
 
     def get_min_dist(self, x, y):
         """
-        Not using one liners to be more clear ;)
-        :param x:
-        :param y:
-        :return:
+        Calculates the minimum distance from point x,y to object - to the closest edge or vertex of object
+        :param x: x parameter of point
+        :param y: y parameter of point
+        :return: distance from point to the box
         """
         if self.xmin <= x <= self.xmax:
             if self.ymin >= y:
@@ -50,6 +56,17 @@ class BndBox:
             x_dist = min(abs(self.xmin - x), abs(self.xmax - x))
             return math.sqrt(y_dist ** 2 + x_dist ** 2)
 
+    def intersect_ratio(self, outer_group):
+        """
+        Returns percentage of self area being inside of outer_group area.
+        :param outer_group: group to check intersect with
+        :return: Percentage of self object being inside of outer_group
+        """
+        x_overlap = max(0, min(self.xmax, outer_group.xmax) - max(self.xmin, outer_group.xmin))
+        y_overlap = max(0, min(self.ymax, outer_group.ymax) - max(self.ymin, outer_group.ymin))
+        overlap_area = x_overlap * y_overlap
+        return float(overlap_area) / self.area
+
 
 class StopDetector:
 
@@ -60,16 +77,18 @@ class StopDetector:
         self.screen_width = 800
         self.screen_height = 600
 
+        self.being_inside_ratio = 0.8
+
         self.max_eu_lights = 3
         # number of frames without vision of lights to clear previous states
         self.not_seen_threshold = 3
         self.prev_states = []
 
-    def detect_actual(self, xml_file):
+    def check_light(self, xml_file):
         """
         Detect light state based on current frame
-        :param xml_file:
-        :return:
+        :param xml_file: xml file with boxes
+        :return: None
         """
         objects = []
         root = ET.parse(xml_file).getroot()
@@ -98,15 +117,18 @@ class StopDetector:
             us_lights = True
 
         if us_lights:
-            return self.check_us(objects)
+            selected = self.check_us(objects)
         else:
-            return self.check_eu(objects)
+            selected = self.check_eu(objects)
+
+        selected_name = selected.name.split("_")[0]
+        self.prev_states.append(selected_name)
 
     def check_eu(self, lights):
         """
-        Method returns current European light states
-        :param lights:
-        :return:
+        Method returns selected European light
+        :param lights: list of all light boxes
+        :return: selected box
         """
         to_remove = []
         for light in lights:
@@ -119,18 +141,112 @@ class StopDetector:
             return None
 
         max_area = -1
-        max_area_object = None
+        biggest_object = None
         for light in lights:
             if light.area > max_area:
                 max_area = light.area
-                max_area_object = light
+                biggest_object = light
 
-        return max_area_object
+        return biggest_object
 
     def check_us(self, lights):
         """
-        Method returns current American light states
-        :param lights:
-        :return:
+        Method returns selected American light
+        :param lights: list of all light boxes
+        :return: selected box
         """
-        return None
+        # groups of lights - us style
+        groups = []
+        for light in lights:
+            if "group" in light.name:
+                groups.append(light)
+        lights -= groups
+
+        # single lights being inside of another group
+        insiders = []
+        for light in lights:
+            for group in groups:
+                if light.intersect_ratio(group) > self.being_inside_ratio:
+                    insiders.append(light)
+                    break
+        lights -= insiders
+        lights += groups
+
+        # minimal distance from center of the screen
+        min_dist = self.screen_width * self.screen_height
+        closest_light = None
+
+        for light in lights:
+            # calculate the distance from center of the screen to the closest edge/vertex of the box
+            distance = light.get_min_dist(self.screen_width / 2, self.screen_height / 2)
+            if distance < min_dist:
+                min_dist = distance
+                closest_light = light
+
+        return closest_light
+
+    def detect_actual(self, states=None):
+        """
+        Method returns actual light state based on previous light states
+        :param states: list of states
+        :return: current light state
+        """
+        if states is None:
+            states = self.prev_states
+
+        if len(states) < 3:
+            return states[-1]
+        else:
+            # yellow - red - red
+            # yellow - yellow - red
+            # green - yellow - red
+            if states[-1] == "red":
+                if states[-2] == "red" and states[-3] == "yellow":
+                    return "red"
+                elif states[-2] == "yellow" and (states[-3] == "yellow" or states[-3] == "green"):
+                    return "red"
+            # green - green - yellow
+            # red - red - yellow
+            # green - yellow - yellow
+            # red - yellow - yellow
+            if states[-1] == "yellow":
+                if (states[-2] == "red" and states[-3] == "red") or (states[-2] == "green" and states[-3] == "green"):
+                    return "yellow"
+                elif states[-2] == "yellow" and (states[-3] == "red" or states[-3] == "green"):
+                    return "yellow"
+            # yellow - green - green
+            # yellow - yellow - green
+            # red - yellow - green
+            if states[-1] == "green":
+                if (states[-2] == "green" or states[-2] == "yellow") and states[-3] == "yellow":
+                    return "green"
+                if states[-2] == "yellow" and states[-3] == "red":
+                    return "green"
+
+            last = {"green": 0, "yellow": 0, "red": 0}
+            for i in range(1, 4):
+                last[states[-i]] += 1
+            # all 3 possible states in last 3 states
+            if max(last.values()) - min(last.values()) == 0:
+                if len(states) > 4:
+                    # the most common state in last 4 is -4 state
+                    return states[-4]
+                else:
+                    # cannot detect current light
+                    return "red"
+            else:
+                return max(last, key=last.get)
+
+    def light_stop(self, xml_file):
+        """
+        Method checking if vehicle should stop based on current traffic light conditions
+        :param xml_file: file with classified boxes
+        :return: True if vehicle must stop or False if not
+        """
+        self.check_light(xml_file)
+        actual = self.detect_actual()
+        if actual is "red":
+            return True
+        else:
+            return False
+
