531d8ea5172a6c4098c48727ed0520404e47b9cf
bwroblew <bwroblewski10@gmail.com>
2019-05-12 22:59:50 +0200

    red-red-green

--- StopDetector.py
+++ StopDetector.py
@@ -225,10 +225,15 @@ class StopDetector:
                     return "Yellow"
                 elif states[-2] == "Yellow" and states[-3] == "Green":
                     return "Yellow"
+            # red - red - green
+            if states[-1] == "green":
+                if states[-2] == "red" and states[-3] == "red":
+                    return "green"
 
             last = {"Green": 0, "Yellow": 0, "Red": 0}
             for i in range(1, 4):
-                last[states[-i]] += 1
+                if states[-i] is not None:
+                    last[states[-i]] += 1
             # all 3 possible states in last 3 states
             if max(last.values()) - min(last.values()) == 0:
                 if len(states) > 3:
